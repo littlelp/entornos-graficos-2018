4f0e775c68828ec2c4fe6528f73d5509296b6af2
Marianela Leoncini <marianela.leoncini@corp.aaj.com>
2018-08-06 17:55:51 -0300

    fix agregar prod

--- nuevo-producto.php
+++ nuevo-producto.php
@@ -8,6 +8,8 @@
 //******************************************************************************
 
 include("includes/conexion.php");
+
+header('Location:listar-productos.php');
 //(isset($_SESSION['tipous'])){
 									
 								//	if($_SESSION['tipous']==1){
@@ -20,22 +22,19 @@ if ($_FILES) {
 	move_uploaded_file($_FILES['Imagen']['tmp_name'], $destino) ;}
 	
 if(isset($_POST["Destacado"])){		
-$sql = "INSERT INTO producto (destacado, nombre, descripcion, video, idCategoria, dimensiones, especificaciones, caracteristicas, precioLista, precio1, precio2, precio3, precio4, imagen)
-		VALUES ('". $_POST["Destacado"]. "','". $_POST["Nombre"]. "','". $_POST["Descripcion"]. "','". $_POST["Video"]. "','". $_POST["cat"]. "','". $_POST["Dimensiones"]. "','". 
-		$_POST["Especificaciones"]. "','". $_POST["Caracteristicas"]. "','". $_POST["PrecioLista"]. "','". $_POST["Precio1"]. "','". $_POST["Precio2"]. "','". $_POST["Precio3"]. "'
-		,'". $_POST["Precio4"]. "','".$destino."')";
+$sql = "INSERT INTO producto (destacado, nombre, descripcion, idCategoria, precioLista, precio1, imagen)
+		VALUES ('". $_POST["Destacado"]. "','". $_POST["Nombre"]. "','". $_POST["Descripcion"]. "','". $_POST["cat"]. "','". $_POST["Caracteristicas"]. "','". $_POST["PrecioLista"]. "','". $_POST["Precio1"]. "',
+		'".$destino."')";
 }
 else{
-	$sql = "INSERT INTO producto (nombre, descripcion, video, idCategoria, dimensiones, especificaciones, caracteristicas, precioLista, precio1, precio2, precio3, precio4, imagen)
-		VALUES ('". $_POST["Nombre"]. "','". $_POST["Descripcion"]. "','". $_POST["Video"]. "','". $_POST["cat"]. "','". $_POST["Dimensiones"]. "','". 
-		$_POST["Especificaciones"]. "','". $_POST["Caracteristicas"]. "','". $_POST["PrecioLista"]. "','". $_POST["Precio1"]. "','". $_POST["Precio2"]. "','". $_POST["Precio3"]. "'
-		,'". $_POST["Precio4"]. "','".$destino."')";
+	$sql = "INSERT INTO producto (nombre, descripcion, idCategoria, precioLista, precio1, imagen)
+		VALUES ('". $_POST["Nombre"]. "','". $_POST["Descripcion"]. "','". $_POST["cat"]. "','". $_POST["Caracteristicas"]. "','". $_POST["PrecioLista"]. "','". $_POST["Precio1"]. "',
+		'".$destino."')";
 }
 		
 $rs = mysqli_query($db, $sql);
 
 
-header('Location:listar-productos.php');
 //}}
 //else {echo'<script language="javascript">window.location="index.php"</script>;';}
 ?>
