12f370a3ca0aef9bd07ffe14efcdbf6c5583b54f
Lucrecia Peralta <lucrecia.peralta@ecloudsolutions.com>
2018-08-26 22:22:30 -0300

    ADD:valida users distintos en el registro

--- registrarse.php
+++ registrarse.php
@@ -2,13 +2,30 @@
 
 include("includes/conexion.php");
 $pass=md5($_POST["contrasena"]);		
-		
-$sql = "INSERT INTO usuario (usuario, contrasena, nombre, apellido, email, tipous, fechaAlta)
-		VALUES ('". $_POST["usuario"]. "','". $pass. "','". $_POST["Nombre"]. "','". $_POST["Apellido"]. "','". $_POST["Email"]. "', 2 , now())";
- 
-		
-$rs = mysqli_query($db, $sql);
+$email = $_POST["Email"];
 
-header('Location:registro-exitoso.php');
+//--------------------------------------------------------------------------------
 
+$sqll = "SELECT email FROM usuario WHERE email = '$email' LIMIT 1";
+
+$res = mysqli_query($db, $sqll) or die(mysql_error());
+
+if (mysqli_num_rows($res) > 0) {
+	echo '
+	  <script type="text/javascript" language="javascript">
+	      window.alert("Ya existe un usuario registrado con ese email.");
+	      window.location.replace("index.php");
+	  </script> ';
+	  //header('Location:usuarios-nueva.php');
+}
+else {
+		
+	$sql = "INSERT INTO usuario (usuario, contrasena, nombre, apellido, email, tipous, fechaAlta)
+			VALUES ('". $_POST["usuario"]. "','". $pass. "','". $_POST["Nombre"]. "','". $_POST["Apellido"]. "','". $_POST["Email"]. "', 2 , now())";
+	 
+			
+	$rs = mysqli_query($db, $sql);
+
+	header('Location:registro-exitoso.php');
+}
 ?>
