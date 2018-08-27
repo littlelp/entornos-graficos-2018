a4efbbd1bb534914773aa30ff622f8c59a42b076
Lucrecia Peralta <lucrecia.peralta@ecloudsolutions.com>
2018-08-26 22:52:30 -0300

    ADD: validacion nombres de usuario diferentes

--- nuevo-user.php
+++ nuevo-user.php
@@ -6,32 +6,52 @@ include("includes/conexion.php");
 //if($_SESSION['tipous']==1){
 $pass=md5($_POST["contrasena"]);	
 $email = $_POST["Email"];
+$usuario = $_POST["usuario"];
 
 //--------------------------------------------------------------------------------
 
-$sqll = "SELECT email FROM usuario WHERE email = '$email' LIMIT 1";
+$sqlEmail = "SELECT email FROM usuario WHERE email = '$email' LIMIT 1";
+$sqlUser = "SELECT usuario FROM usuario WHERE usuario = '$usuario' LIMIT 1";
 
-$res = mysqli_query($db, $sqll) or die(mysql_error());
 
-if (mysqli_num_rows($res) > 0) {
+$res2 = mysqli_query($db, $sqlUser) or die(mysql_error());
+$registros= mysqli_num_rows($res);
+$registros2= mysqli_num_rows($res2);
+
+if ($registros > 0 && $registros2 > 0) {
 	echo '
 	  <script type="text/javascript" language="javascript">
-	      window.alert("Ya existe un usuario registrado con ese email.");
-	      window.location.replace("usuarios-nueva.php");
+	      window.alert("El nombre de usuario y el email ingresados ya se encuentran registrados.");
+	      window.location.replace("index.php");
 	  </script> ';
 	  //header('Location:usuarios-nueva.php');
 }
 else {
-		
-	$sql = "INSERT INTO usuario (usuario, contrasena, nombre, apellido, email, tipous, fechaAlta)
-			VALUES ('". $_POST["usuario"]. "','". $pass. "','". $_POST["Nombre"]. "','". $_POST["Apellido"]. "','". $_POST["Email"]. "','". $_POST["tipouser"]. "', now())";
-	 
-			
-	$rs = mysqli_query($db, $sql);
+	if ($registros > 0) {
+		echo '
+		<script type="text/javascript" language="javascript">
+	      window.alert("Ya existe un usuario registrado con ese email.");
+	      window.location.replace("index.php");
+	  </script> ';
+	} 
+	else {
+		if ($registros2 > 0) {
+			echo '
+			<script type="text/javascript" language="javascript">
+		      window.alert("Ya existe un usuario registrado con ese nombre de usuario.");
+		      window.location.replace("index.php");
+		  	</script> ';
+		} else {
+			$sql = "INSERT INTO usuario (usuario, contrasena, nombre, apellido, email, tipous, fechaAlta)
+					VALUES ('". $_POST["usuario"]. "','". $pass. "','". $_POST["Nombre"]. "','". $_POST["Apellido"]. "','". $_POST["Email"]. "','". $_POST["tipouser"]. "', now())";
+			 
+					
+			$rs = mysqli_query($db, $sql);
 
-	//}}
-	//else {echo'<script>window.location="index.php"</script>;';}
-	header('Location:listar-usuarios.php');
+			//}}
+			//else {echo'<script>window.location="index.php"</script>;';}
+			header('Location:listar-usuarios.php');
+		}
+	}	
 }
-
 ?>
--- registrarse.php
+++ registrarse.php
@@ -3,29 +3,50 @@
 include("includes/conexion.php");
 $pass=md5($_POST["contrasena"]);		
 $email = $_POST["Email"];
+$usuario = $_POST["usuario"];
 
 //--------------------------------------------------------------------------------
 
-$sqll = "SELECT email FROM usuario WHERE email = '$email' LIMIT 1";
+$sqlEmail = "SELECT email FROM usuario WHERE email = '$email' LIMIT 1";
+$sqlUser = "SELECT usuario FROM usuario WHERE usuario = '$usuario' LIMIT 1";
 
-$res = mysqli_query($db, $sqll) or die(mysql_error());
 
-if (mysqli_num_rows($res) > 0) {
+$res = mysqli_query($db, $sqlEmail) or die(mysql_error());
+$res2 = mysqli_query($db, $sqlUser) or die(mysql_error());
+$registros= mysqli_num_rows($res);
+$registros2= mysqli_num_rows($res2);
+
+if ($registros > 0 && $registros2 > 0) {
 	echo '
 	  <script type="text/javascript" language="javascript">
-	      window.alert("Ya existe un usuario registrado con ese email.");
+	      window.alert("El nombre de usuario y el email ingresados ya se encuentran registrados.");
 	      window.location.replace("index.php");
 	  </script> ';
 	  //header('Location:usuarios-nueva.php');
 }
 else {
-		
-	$sql = "INSERT INTO usuario (usuario, contrasena, nombre, apellido, email, tipous, fechaAlta)
+	if ($registros > 0) {
+		echo '
+		<script type="text/javascript" language="javascript">
+	      window.alert("Ya existe un usuario registrado con ese email.");
+	      window.location.replace("index.php");
+	  </script> ';
+	} 
+	else {
+		if ($registros2 > 0) {
+			echo '
+			<script type="text/javascript" language="javascript">
+		      window.alert("Ya existe un usuario registrado con ese nombre de usuario.");
+		      window.location.replace("index.php");
+		  	</script> ';
+		} else {
+			$sql = "INSERT INTO usuario (usuario, contrasena, nombre, apellido, email, tipous, fechaAlta)
 			VALUES ('". $_POST["usuario"]. "','". $pass. "','". $_POST["Nombre"]. "','". $_POST["Apellido"]. "','". $_POST["Email"]. "', 2 , now())";
 	 
-			
-	$rs = mysqli_query($db, $sql);
+			$rs = mysqli_query($db, $sql);
 
-	header('Location:registro-exitoso.php');
+			header('Location:registro-exitoso.php');
+		}
+	}	
 }
 ?>
